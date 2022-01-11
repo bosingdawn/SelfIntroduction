58686c67d5202dc950f261f884608f9ada6054ca
bosingdawn <97539667+bosingdawn@users.noreply.github.com>
2022-01-11 09:23:33 -0700

    Add files via upload

---
+++ Introduction.java
@@ -0,0 +1,13 @@
+public class Introduction {
+    public static void main(String[] args) {
+        String myName = "Dawn";
+        String mySchool = "ABM College";
+        String myCourse = "Mobile App Development";
+        int myAge = 20;
+
+        System.out.println("My Name is " + myName);
+        System.out.println("I am attending school at " + mySchool);
+        System.out.println("The course that I am currently taking is " + myCourse);
+        System.out.println("Lastly, I am " + myAge + " years old.");
+    }
+}
